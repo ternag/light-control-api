6b8928e4b81047b2c03ed6c6e7caa110585c1a29
terje nagel <terje.nagel@gmail.com>
2020-03-19 18:00:52 +0100

    Renamed src folder

---
+++ src/Controllers/LedController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using LightControl.Api.Models;
+using LightControl.Api.Infrastructure;
+
+namespace LightControl.Api.Controllers
+{
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class LedController : ControllerBase
+    {
+        private readonly ILogger<LedController> _logger;
+        private readonly ILedContext _ledContext;
+        private readonly IHal _hal;
+
+        public LedController(ILedContext ledContext, ILogger<LedController> logger, IHal hal)
+        {
+            _logger = logger;
+            _ledContext = ledContext;
+            _hal = hal;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Led>> Get()
+        {
+            return CatchExceptions<IEnumerable<Led>>(() => _ledContext.All);
+        }
+
+
+        [HttpGet]
+        [Route("{id}")]
+        public ActionResult<Led> Get(int id)
+        {
+            _logger.LogInformation($"Getting LED {id}");
+
+            return CatchExceptions<Led>(() => _ledContext.Get(id));
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public ActionResult<Led> Put(int id, [FromBody]LedUpdateDisplay newDisplayValue)
+        {
+            _logger.LogInformation($"Updating LED {id}, display={newDisplayValue.Display}");
+
+            return CatchExceptions<Led>(() => {
+                    Led knownLed = _ledContext.Get(id);
+                    knownLed.Display = newDisplayValue.Display;
+                    return knownLed;
+                });
+        }
+
+        [HttpGet]
+        [Route("{id}/_flick")]
+        public ActionResult<Led> Flick(int id)
+        {
+            _logger.LogInformation($"Flicking LED {id}");
+
+            return CatchExceptions<Led>(() => FlickAndUpdate(id));
+        }
+
+        private Led FlickAndUpdate(int id)
+        {
+            var led = _ledContext.Flick(id);
+            _hal.Update(led);
+            return led;
+        }
+
+        private ActionResult<T> CatchExceptions<T>(Func<T> method)
+        {
+            try
+            {
+                return (T)method();
+            }
+            catch (ArgumentException ae)
+            {
+                return NotFound(ae.Message);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+    }
+
+}
---
+++ src/Startup.cs
@@ -0,0 +1,75 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using LightControl.Api.Infrastructure;
+
+namespace LightControl.Api
+{
+  public class Startup
+  {
+    private IWebHostEnvironment _env;
+
+    public Startup(IConfiguration configuration, IWebHostEnvironment env)
+    {
+      Configuration = configuration;
+      _env = env;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddControllers();
+      services.AddSingleton(typeof(ILogger<>), typeof(Logger<>));
+      services.AddSingleton<ILedContext, LedContext>();
+      
+      // Registre Hardware Abstraction Layer dependent on environment
+      if (_env.IsDevelopment())
+      {
+        services.AddSingleton<IHal>((container) =>
+        {
+          var logger = container.GetRequiredService<ILogger<NoHardwareHAL>>();
+          return new NoHardwareHAL(logger);
+        });
+      }
+      else
+      {
+        services.AddSingleton<IHal>((container) =>
+        {
+          var logger = container.GetRequiredService<ILogger<RaspberryPiGpioHAL>>();
+          return new RaspberryPiGpioHAL(logger);
+        });
+      }
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app)
+    {
+      if (_env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+
+      //app.UseHttpsRedirection();
+
+      app.UseRouting();
+
+      app.UseForwardedHeaders(new ForwardedHeadersOptions
+      {
+          ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+      });
+
+      //app.UseAuthorization();
+
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapControllers();
+      });
+    }
+  }
+}
