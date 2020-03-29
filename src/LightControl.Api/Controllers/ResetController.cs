970668a91dc6ab8e9367f79e186ea9dfb7286915
Terje Nagel <terje.nagel@gmail.com>
2020-03-30 00:25:47 +0200

    Added ResetController

---
+++ src/LightControl.Api/Controllers/ResetController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LightControl.Api.Controllers
+{
+  [ApiController]
+  [Route("/api/[controller]")]
+  public class ResetController : Controller
+  {
+    // GET
+    public ActionResult<bool> Index()
+    {
+      return false;
+    }
+  }
+}
